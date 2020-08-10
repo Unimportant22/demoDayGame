b35dd2e5876c3a0e0a837e30180b51aa4aaafaf7
unimportant22 <ajemerenciano3@gmail.com>
2020-08-10 19:57:31 -0400

    Hi

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let img;
+
+function preload(){
+    img = loadImage("background.jpg");
+}
+
+function setup(){
+    createCanvas(700,700);
+    //Set background for Game
+    image(img, 350, 350, 350, 350);
+}
+
+function draw(){
+    //Player char is a red dot
+
+    //Update position using keyboard buttons
+
+    //Add random events in map, smthn  to be done after above
+
+}
